7401415296f8f3035f983b3de138a93bfb3133bf
mcgleec2 <cormac.mcgleenan2@mail.dcu.ie>
2025-02-09 10:44:47 +0000

    Code for hello world program from C Programming Primer notes

---
+++ week3/hello_mpi.c
@@ -0,0 +1,9 @@
+// include the standard input/output library
+#include <stdio.h>
+
+// define the main() function to return an int
+int main()
+{
+	printf("Hello, World!\n");
+	return 0; // return 0 by default
+}
