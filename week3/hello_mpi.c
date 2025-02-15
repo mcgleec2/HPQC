689c5ab2f4bb843afa0e5437457f2560eea27d8b
mcgleec2 <cormac.mcgleenan2@mail.dcu.ie>
2025-02-15 11:18:42 +0000

    Program from PHY1090/week3

--- week3/hello_mpi.c
+++ week3/hello_mpi.c
@@ -1,28 +1,26 @@
-// include the standard input/output library
 #include <stdio.h>
-
-// include the MPI header file
 #include <mpi.h>
 
-// define the main() function to return an int
-int main(int argc, char *argv[])
+int main(int argc, char **argv) 
 {
-	// initialise the MPI environment
-	MPI_Init(&argc,& argv);
+	// declare and initialise error handling variable
+	int ierror = 0;
+	
+	// declare and initialise rank and size varibles
+	int my_rank, uni_size;
+	my_rank = uni_size = 0;
 
-	// get the rank of the process
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	// intitalise MPI
+	ierror = MPI_Init(&argc, &argv);
 
-	// get the total number of processes
-	int size;
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	// gets the rank and world size
+	ierror = MPI_Comm_rank(MPI_COMM_WORLD,&my_rank);
+	ierror = MPI_Comm_size(MPI_COMM_WORLD,&uni_size);
 
-	// print "Hello, World!" with the rank of each process
-	printf("Hello, World! from process %d out of %d processes\n", rank, size);
+	// prints to the screen
+	printf("Hello, I am %d of %d\n",my_rank, uni_size);
 
-	// finalise the MPI environment
-	MPI_Finalize();
-
-	return 0; // return 0 by default
+	// finalise MPI
+	ierror = MPI_Finalize();
+	return 0;
 }
