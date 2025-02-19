343ddf4dce73690421ebe2631f130434357e3f36
mcgleec2 <cormac.mcgleenan2@mail.dcu.ie>
2025-02-19 19:02:28 +0000

    Copied own from PHY1090

---
+++ week4/README.md
@@ -0,0 +1,16 @@
+Week 4 Practical
+
+Program Tracking:
+Part 1:
+Step 1:
+comm_test_mpi.c = PHY1090/week4 original program
+Step 2:
+comm_test_mpi1.c = Functionlised program 
+Step 3:
+comm_test_mpi1_S.c = Modified comm_test_mpi1.c to use MPI_Ssend()
+comm_test_mpi1_B.c = Modified comm_test_mpi1.c to use MPI_Bsend()
+comm_test_mpi1_R.c = Modified comm_test_mpi1.c to use MPI_Rsend()
+comm_test_mpi1_I.c = Modified comm_test_mpi1.c to use MPI_Isend()
+Part 2:
+Step 1:
+
---
+++ week4/comm_test_mpi1_B.c
@@ -0,0 +1,114 @@
+// Using MPI_Bsend()
+
+#include <stdio.h>
+#include <mpi.h>
+
+// functions
+void initialize_mpi(int *argc, char ***argv, int *my_rank, int *uni_size);
+void finalize_mpi();
+void send_message_to_root(int my_rank, int uni_size);
+void receive_message_from_others(int my_rank, int uni_size);
+void handle_communication(int my_rank, int uni_size);
+
+int main(int argc, char **argv)
+{
+    // declares variables
+    int my_rank, uni_size;
+
+    // initialises MPI
+    initialize_mpi(&argc, &argv, &my_rank, &uni_size);
+
+    // handles communication (send/receive messages)
+    handle_communication(my_rank, uni_size);
+
+    // finalises MPI
+    finalize_mpi();
+
+    return 0;
+}
+
+// function to initialise MPI
+void initialize_mpi(int *argc, char ***argv, int *my_rank, int *uni_size)
+{
+    int ierror = MPI_Init(argc, argv);
+
+    // gets rank and world size
+    ierror = MPI_Comm_rank(MPI_COMM_WORLD, my_rank);
+    ierror = MPI_Comm_size(MPI_COMM_WORLD, uni_size);
+}
+
+// function to finalise MPI
+void finalize_mpi()
+{
+    int ierror = MPI_Finalize();
+}
+
+// function to handle sending messages from non-root processes using MPI_Bsend()
+void send_message_to_root(int my_rank, int uni_size)
+{
+    int send_message = my_rank * 10; // creates the message to send
+    int dest = 0;                    // root rank is 0
+    int tag = 0;
+    int count = 1;
+
+    // gives buffer for message
+    int buffer[1];
+
+    // copies message into buffer
+    buffer[0] = send_message;
+
+    // enables use of buffered sends
+    MPI_Buffer_attach(buffer, sizeof(buffer));
+
+    // sends message to rank 0 using MPI_Bsend
+    MPI_Bsend(&send_message, count, MPI_INT, dest, tag, MPI_COMM_WORLD);
+
+    // prints message
+    printf("Hello, I am %d of %d. Sent %d to Rank %d\n", my_rank, uni_size, send_message, dest);
+}
+
+// function to handle receiving messages from other processes
+void receive_message_from_others(int my_rank, int uni_size)
+{
+    int recv_message;
+    int source;
+    int count = 1;
+    int tag = 0;
+    MPI_Status status;
+
+    // iterates through all ranks except root (rank 0)
+    for (int their_rank = 1; their_rank < uni_size; their_rank++)
+    {
+        source = their_rank; // sets source rank of the message
+
+        // receives the message
+        MPI_Recv(&recv_message, count, MPI_INT, source, tag, MPI_COMM_WORLD, &status);
+
+        // prints the received message
+        printf("Hello, I am %d of %d. Received %d from Rank %d\n", my_rank, uni_size, recv_message, source);
+    }
+}
+
+// function to handle communication
+void handle_communication(int my_rank, int uni_size)
+{
+    if (uni_size > 1)
+    {
+        if (my_rank == 0)
+        {
+            // if rank is 0 (root), receives messages from other ranks
+            receive_message_from_others(my_rank, uni_size);
+        }
+        else
+        {
+            // if rank is not 0 (not root), sends message to rank 0
+            send_message_to_root(my_rank, uni_size);
+        }
+    }
+    else
+    {
+        // prints a warning
+        printf("Unable to communicate with less than 2 processes. MPI communicator size = %d\n", uni_size);
+    }
+}
+
